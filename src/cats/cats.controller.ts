71af96531b27744df69c19b6bc24e6e59f49c818
LEE JUN HEE <wnsgml351@naver.com>
2024-08-31 22:59:56 +0900

    nest js 프로젝트 추가 및 middleware 추가

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,37 @@
+import { CatsService } from './cats.service';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+    constructor(private readonly CatsService: CatsService) {}
+
+    @Get()
+    getAllCat() {
+        return 'all cat';
+    }
+
+    @Get(':id')
+    getOneCat() {
+        return 'one cat';
+    }
+
+    @Post()
+    createCat() {
+        return 'create cat';
+    }
+
+    @Put(':id')
+    updateCat() {
+        return 'update cat';
+    }
+
+    @Patch(':id')
+    updatePartialCat() {
+        return 'update';
+    }
+
+    @Delete(':id')
+    deleteCat() {
+        return 'delete cat';
+    }
+}
