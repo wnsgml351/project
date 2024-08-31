4b82fdae4df2aa2901f65499a5e9c66349c47103
LEE JUN HEE <wnsgml351@naver.com>
2024-09-01 00:03:36 +0900

    HttpExceptionFilter

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,18 +1,34 @@
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { CatsService } from './cats.service';
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+    Controller,
+    Delete,
+    Get,
+    HttpException,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post,
+    Put,
+    UseFilters,
+} from '@nestjs/common';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
     constructor(private readonly CatsService: CatsService) {}
 
     @Get()
     getAllCat() {
+        throw new HttpException('api is broken', 401);
         return 'all cat';
     }
 
     @Get(':id')
-    getOneCat() {
-        return 'one cat';
+    getOneCat(@Param('id', ParseIntPipe) param: number) {
+        console.log(param);
+        console.log(typeof param);
+        return 'get one cat api';
     }
 
     @Post()
